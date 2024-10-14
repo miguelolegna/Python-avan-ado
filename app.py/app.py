412d047fc7fa067be16c774a6ec97e28f2612573
antunes478 <portuga0462016@gmail.com>
2024-10-14 15:51:31 +0100

    alteracao nos styless de cada elemento html

--- app.py/app.py
+++ app.py/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 from flask_assets import Environment, Bundle
 
@@ -11,34 +10,39 @@ assets = Environment(app)
 scss = Bundle('scss/styles.scss', filters='libsass', output='css/styles.css')
 assets.register('scss_all', scss)
 
-@app.route('/')  # URL: http://127.0.0.1:5000/index
+# Rotas
+@app.route('/') 
 def index():
     return render_template('index.html')
 
-@app.route('/java')   # URL: http://127.0.0.1:5000/JAVA
+@app.route('/java')  
 def java():
     return render_template('java.html')
 
-@app.route('/python') # URL: http://127.0.0.1:5000/python
+@app.route('/python') 
 def python():
     return render_template('python.html')
 
-@app.route('/cpp')    # URL: http://127.0.0.1:5000/cpp
-
+@app.route('/cpp')    
 def cpp():
     return render_template('cpp.html')
 
-@app.route('/db')     # URL: http://127.0.0.1:5000/db
+@app.route('/db')    
 def database():
     return render_template('db.html')
 
-@app.route('/reds')  # URL: http://127.0.0.1:5000/redes
+@app.route('/reds')  
 def networks():
     return render_template('reds.html')
 
-@app.route('/pross-comp')  # URL: http://127.0.0.1:5000/pross-comp
+@app.route('/pross-comp')  
 def computing():
     return render_template('pross-comp.html')
 
+@app.route('/Documentação')  
+def documentacao():
+    return render_template('Documentação.html')
+
+# Iniciar o app
 if __name__ == '__main__':
     app.run(debug=True)
