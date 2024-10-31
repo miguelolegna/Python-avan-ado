0d8c418b3afadf6e92937724d5f7731b80ff1977
JeanPierrePro <jeanpierreaprogramador@gmail.com>
2024-10-31 12:10:30 +0000

    todos os htmls em um

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Página Inicial")
+    return render_template('index.html', title="Portal de Tecnologia e Computação")
 
 @app.route('/cpp')
 def cpp():
