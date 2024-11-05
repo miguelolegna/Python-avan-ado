fd6a718a4fa6b70bd1bd24334058ee160687217d
JeanPierrePro <jeanpierreaprogramador@gmail.com>
2024-11-05 00:06:41 +0000

    Modificacoes gerais

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projeto')
+def projeto():
+    return render_template('projeto.html')  # Adicione a página 'projeto.html' correspondente
+
+@app.route('/documentacao')
+def documentacao():
+    return render_template('documentacao.html')  # Adicione a página 'documentacao.html' correspondente
+
+@app.route('/modulos')
+def modulos():
+    return render_template('modulos.html')
+
+@app.route('/processamento')
+def processamento():
+    return render_template('processamento.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
